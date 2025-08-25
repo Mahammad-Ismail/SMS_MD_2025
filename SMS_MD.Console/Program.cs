402a2fe90c51e01d2df7239c0924cef7c6bc8c8f
Mahammad-Ismail <mahammad1014@outlook.com>
2025-08-25 22:18:25 +0530

    Added custom constructor and methods in console project

--- SMS_MD.Console/Program.cs
+++ SMS_MD.Console/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+using SMS_MD.Console;
+
 Console.WriteLine("Hello, World!");
+User defaultuser = new User();
+
+defaultuser.Greet();
+
+User customuser = new User("Ismail",25);
+string greetingmsg=customuser.Greetwithreturn();
+Console.WriteLine(greetingmsg);
+
+string custommesg = customuser.Greetwithreturn("Abdul", 30);
+Console.WriteLine(custommesg);
+Console.ReadLine();
---
+++ SMS_MD.Console/User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS_MD.Console
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public User() 
+        {
+          Name = "Mahammad";
+            Age = 21;
+        }
+        public User(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public void Greet()
+        {
+            System.Console.WriteLine($"Hello, my name is {Name} and I am {Age} years old.");
+        }
+        public string Greetwithreturn()
+        {
+            string result = $"Hello, my name is {Name} and I am {Age} years old.";
+            return result;
+        }
+        public string Greetwithreturn(string name, int age)
+        {
+            string result = $"Hello, my name is {name} and I am {age} years old.";
+            return result;
+        }
+
+    }
+}
