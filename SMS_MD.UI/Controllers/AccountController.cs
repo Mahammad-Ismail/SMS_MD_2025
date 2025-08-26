08e9e4b35cb42b8abbf7cac1a85f0f8a7f5f50c7
Mahammad-Ismail <mahammad1014@outlook.com>
2025-08-26 14:43:06 +0530

    Added AccountController and login method in it, footer (razor page ) created in shared folder.

---
+++ SMS_MD.UI/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SMS_MD.UI.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
